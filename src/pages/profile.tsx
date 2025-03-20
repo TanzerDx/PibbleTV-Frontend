3809084ac098a4bc21a3ceed71aebf1ee1bfd890
TanzerDx <hristoganchev3@gmail.com>
2025-03-20 10:00:48 +0100

    fixed homepage and categories page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+interface User {
+  uid: string;
+  username: string;
+  email: string;
+  profilePic: string;
+  bgPic: string;
+  followers: number;
+  following: number;
+}
+
+const Profile: React.FC = () => {
+  const [userDetails, setUserDetails] = useState<User | null>(null);
+
+  return userDetails ? (
+    <div>
+      <div>
+        <img
+          className="w-full h-[30vh] object-cover"
+          src={userDetails.bgPic}
+          alt="Background"
+        />
+      </div>
+
+      <div className="flex p-10">
+        <div className="w-10%">
+          <img
+            className="w-full rounded-full"
+            src={userDetails.profilePic}
+            alt="ProfilePic"
+          />
+        </div>
+
+        <div className="flex flex-row items-center ml-2%">
+          <div>
+            <h1 className="text-3xl font-bold">{userDetails.username}</h1>
+          </div>
+          <div className="flex flex-col ml-2%">
+            <h1 className="text-3xl">{userDetails.followers} followers </h1>
+            <h1 className="text-3xl">{userDetails.following} following</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">Loading...</h1>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/services/AuthenticationService.tsx
@@ -0,0 +1,31 @@
+import axios, { AxiosResponse } from "axios";
+
+const hostname = "http://localhost:8080";
+
+function login(email: string, password: string): Promise<string> {
+  return axios
+    .post<string>(`${hostname}/auth/login`, { email, password })
+    .then((response: AxiosResponse<string>) => response.data);
+}
+
+function register(
+  username: string,
+  email: string,
+  password: string,
+  defaultBackgroundPic: string,
+  defaultProfilePic: string
+): Promise<string> {
+  return axios
+    .post<string>(`${hostname}/auth/register`, {
+      username,
+      email,
+      password,
+      defaultBackgroundPic,
+      defaultProfilePic,
+    })
+    .then((response: AxiosResponse<string>) => {
+      return response.data;
+    });
+}
+
+export { login, register };
